7e38d596b9c380c081d2aec5970f3ae945e1c6be
ya8796202-dotcom <ya8796202@gmail.com>
2025-11-22 16:26:17 +0200

    Update sw.js
    Signed-off-by: ya8796202-dotcom <ya8796202@gmail.com>

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  'ALGAZ/index.html',
-  'ALGAZ/manifest.json',
-  'ALGAZ/icons/icon-192.png',
-  'ALGAZ/icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
+// sw.js
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open("algaz-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
